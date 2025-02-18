e5a2cf1a5f7756ece847b2ecf2f3bc56e0038d3e
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-02-18 11:02:47 -0300

    feat: add button and icon-button components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import { Button } from "@/components/button";
+import { IconButton } from "@/components/icon-button";
+import { ArrowRight, Copy } from "lucide-react";
+
 export default function Home() {
   return (
     <main>
-      <div className="">Hello world!</div>
+      <Button>
+        Enviar
+        <ArrowRight />
+      </Button>
+
+      <IconButton>
+        <Copy />
+      </IconButton>
     </main>
   );
 }
